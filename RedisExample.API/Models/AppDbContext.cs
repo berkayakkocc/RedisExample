6b5c29b650b5ed65fcdd59f8d8bcae76405fa35c
Berkaycan Akkoç <berkayakkocc6@gmail.com>
2023-03-25 12:03:26 +0300

    Add DbContext & Product Entity

---
+++ RedisExample.API/Models/AppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RedisExample.API.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product()
+                {
+                    Id = 1,
+                    Name = "Kalem 1",
+                },
+                  new Product()
+                  {
+                      Id = 2,
+                      Name = "Kalem 2",
+                  },
+                     new Product()
+                     {
+                         Id = 3,
+                         Name = "Kalem 3",
+                     });
+
+        }
+    }
+}
