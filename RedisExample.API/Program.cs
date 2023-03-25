ab5101c7b44200d0ff69251f2510e2106988e7de
Berkaycan Akkoç <berkayakkocc6@gmail.com>
2023-03-25 12:25:45 +0300

    Repository&Controller Added

--- RedisExample.API/Program.cs
+++ RedisExample.API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using RedisExample.API.Models;
+using RedisExample.API.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,12 +11,20 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseInMemoryDatabase("myDatabase");
 });
 
+
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
